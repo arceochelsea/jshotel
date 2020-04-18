fd751033044c475feaf053888bfe3ac066b1adbc
Chelsea Arceo <arceo.chelsea@gmail.com>
2020-04-18 18:29:32 -0400

    Completed Hacker Challenge

--- jshotel.js
+++ jshotel.js
@@ -1,16 +1,19 @@
 
 document.getElementsByClassName("slogan")[0].innerText = "SLEEP ALL DAY. SLEEP ALL NIGHT."
 
-//document.getElementsByName("rooms")[0].value = 210;
+document.getElementById("rooms")[0].innerText = "210";
+document.getElementById("rooms")[1].innerText = "211";
+document.getElementById("rooms")[2].innerText = "212";
 
-let roomDropDown = {
-optionOne: 101,
-optionTwo: 102,
-optionThree: 103,
-optionFour: 104,
-optionFive: 105
-};
 
-roomDropDown.optionOne = 210;
-roomDropDown.optionTwo = 211;
-roomDropDown.optionThree = 212;
+// let roomDropDown = {
+// optionOne: 101,
+// optionTwo: 102,
+// optionThree: 103,
+// optionFour: 104,
+// optionFive: 105
+// };
+
+// roomDropDown.optionOne = 210;
+// roomDropDown.optionTwo = 211;
+// roomDropDown.optionThree = 212;
