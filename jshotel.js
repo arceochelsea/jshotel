0d34cab067f99cff5eaff4293ee99ba8320136fc
Chelsea Arceo <arceo.chelsea@gmail.com>
2020-04-18 16:28:58 -0400

    Added js file

---
+++ jshotel.js
@@ -0,0 +1,16 @@
+
+document.getElementsByClassName("slogan")[0].innerText = "SLEEP ALL DAY. SLEEP ALL NIGHT."
+
+//document.getElementsByName("rooms")[0].value = 210;
+
+let roomDropDown = {
+optionOne: 101,
+optionTwo: 102,
+optionThree: 103,
+optionFour: 104,
+optionFive: 105
+};
+
+roomDropDown.optionOne = 210;
+roomDropDown.optionTwo = 211;
+roomDropDown.optionThree = 212;
