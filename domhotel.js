c8cbcb578b5eff3b6f10d0c085dfa1146508c74f
Chelsea Arceo <arceo.chelsea@gmail.com>
2020-04-26 22:07:14 -0400

    Added object and book room function

--- domhotel.js
+++ domhotel.js
@@ -1,16 +1,46 @@
 document.getElementById('hotelName').innerText = "The DOM hotel";
 document.getElementById('slogan').innerText = "Our Customers are the OBJECTS of our desire (no creepy)"
 
-let availableRooms = [101, 102, 103, 104];
 
-// let linesOfHtml = "<select> <option value='101'>101</option> </select>";
+let domHotel = { //object of properties 
+    availableRooms: [ [101, 102, 103, 104], [201], [], [401, 402] ], 
+    bookedRooms: [ [], [], [], [] ],
+    roomTypes: ["Single", "Double", "Queens", "King"],
 
-let linesOfHtml = "<select>";
-for (let i = 0; i < availableRooms.length; i++) {
-linesOfHtml = linesOfHtml + `<option value='${availableRooms[i]}'>${availableRooms[i]}</option>`;  
+    bookRoom: function () {
+        this.bookedRooms[0].push(this.availableRooms[0].pop());
+        this.updateAvailableRoomsInTheDom();
+    },
+
+    // unbookRoom: function () {
+        
+    // }
+
+    updateAvailableRoomsInTheDom: function () {
+        let linesOfHtml = "<select>";
+        for (let i = 0; i < this.availableRooms.length; i++) {
+        for (let j = 0; j < this.availableRooms[i].length; j++) { 
+        linesOfHtml = linesOfHtml + `<option value='${this.availableRooms[i][j]}'>${this.availableRooms[i][j]}</option>`;  
+    }
 }
-linesOfHtml = linesOfHtml + "</select>";
+        linesOfHtml = linesOfHtml + "</select>";
+        document.getElementById('AvailRoomList').innerHTML = linesOfHtml;
+    }
+}
 
-console.log(linesOfHtml);
+domHotel.updateAvailableRoomsInTheDom(); //updates avail rooms in dom, called once when the function begins
+domHotel.bookRoom(); //extra credit is sending a number 
 
-document.getElementById('AvailRoomList').innerHTML = linesOfHtml;
+// let availableRooms = [101, 102, 103, 104];
+
+// // let linesOfHtml = "<select> <option value='101'>101</option> </select>";
+
+// let linesOfHtml = "<select>";
+// for (let i = 0; i < availableRooms.length; i++) {
+// linesOfHtml = linesOfHtml + `<option value='${availableRooms[i]}'>${availableRooms[i]}</option>`;  
+// }
+// linesOfHtml = linesOfHtml + "</select>";
+
+// console.log(linesOfHtml);
+
+// document.getElementById('AvailRoomList').innerHTML = linesOfHtml;
