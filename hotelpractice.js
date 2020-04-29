858d4a0df911f14133e6033813f14f505bc4b632
Chelsea Arceo <arceo.chelsea@gmail.com>
2020-04-28 23:51:48 -0400

    Added functional button to remove available rooms from array to booked array

--- hotelpractice.js
+++ hotelpractice.js
@@ -1,6 +1,16 @@
 document.getElementById('hotelname').innerText = "Holiday Inn";
 document.getElementById('hotelslogan').innerText = "What chu doing? CHILLIN' AT THE HOLIDAY INN!";
 
+
+let bookButton = document.createElement('button');
+bookButton.innerText = 'Book this room!'; //will now display in button
+document.body.appendChild(bookButton); // button will be created in html
+// document.querySelector('bookDiv').appendChild(bookButton); an attempt to move it to bookDiv div but unsuccessful
+
+let unBookButton = document.createElement('button');
+unBookButton.innerText = 'Unbook this room!';
+document.body.appendChild(unBookButton);
+
 let bookingDetails = {
     typeOfRoom: ['Single', 'Double', 'Queen', 'King'],
     availableRooms: [  [102, 103, 107], [204, 211, 212], [], [456] ],
@@ -38,7 +48,7 @@ let bookingDetails = {
         }
         linesOfHtmlTwo = linesOfHtmlTwo + "</select>";    
         document.getElementById('dropdownbookedrooms').innerHTML = linesOfHtmlTwo;
-    }
+    },
 }
 
 bookingDetails.updateAvailableRooms();
